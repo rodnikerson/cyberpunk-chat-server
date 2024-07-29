0be9bddf0855615260df6851c4762502b814c769
rodnikerson <rodolpho.oliveira@acct.global>
2024-07-28 21:40:27 -0300

    fix: vercel config and new port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './app.js'
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
